f83b3d8b93c53f4e4728ea6e45384ac35270bf0f
shihyanmelody <40605494+shihyanmelody@users.noreply.github.com>
2018-08-06 15:53:54 -0500

    Update select_and_print.py

--- select_and_print.py
+++ select_and_print.py
@@ -1,10 +1,12 @@
 import sqlparse
 import csv
+from csv import reader
+import re
 import time
+from prettytable import PrettyTable
 
 
-
-# def Main():
+def Main():
 
 	#sql_statement = "SELECT M.title_year, M.movie_title, A.Award, M.imdb_score, M.movie_facebook_likes FROM movies.csv M JOIN oscars.csv A ON (M.movie_title = A.Film) WHERE A.Winner = 1 AND (M.imdb_score < 6 OR M.movie_facebook_likes < 10000)"
 	#sql_statement = "SELECT movie_title, title_year, imdb_score FROM movies.csv WHERE director_name = 'Ang Lee' AND imdb_score > 7"
@@ -18,10 +20,10 @@ import time
 	#rowindice_result_from_selection = [[2564, 7172, 5129, 8202, 9749]]
 	#rowindice_result_from_selection =[[9590], [9595]]
 	#rowindice_result_from_selection =[[3959, 3989, 3989, 3870, 3671, 3787, 3568, 4075, 3862], [2512, 2760, 2762, 2075, 787, 3961, 5301, 932, 3717], [2522, 2771, 2771, 2085, 800, 3970, 5312, 940, 3727]]
-	# sql_statement = "SELECT M.movie_title FROM movies.cscv M"
-	# alias_index_result = ['M']
-	# rowindice_result_from_selection =[[3787, 3568, 4075, 5862]]
-	# ProjectAndPrint(sql_statement, rowindice_result_from_selection, alias_index_result)
+	sql_statement = "SELECT M.movie_title, M.title_year FROM movies.cscv M"
+	alias_index_result = ['M']
+	rowindice_result_from_selection =[[70585, 725541, 705334, 677902]]
+	ProjectAndPrint(sql_statement, rowindice_result_from_selection, alias_index_result)
 
 
 
@@ -46,10 +48,7 @@ def ProjectAndPrint(sql_statement, rowindice_result_from_selection, alias_index_
 # the function to print the final table line by line, list_name is the statement representing the final table
 def printlist(list_name):
 	for i, row in enumerate(list_name):
-		r = []
-		for s in row:
-			r.append(s.encode('utf-8'))
-		print(r)
+		print(row)
 
 
 # the function the parse the SELECT part (before FROM) with sql statement as the input. If the SELECT is not *, the outpub is a list of alias (can be None is no alias provided) and a list of the corresponding attribute. If the SELECT is *, will return alias_list as [None] and attribute list as [-1], this * statement will be check again when pull out tuple and attribute from the csv document again. The sequence is determined by the appearance in the SELECT part.
@@ -205,12 +204,14 @@ def Findvalueincsv(filename, row_indice, volume_value_list):
 
 def Findvalueincsv_offset(filename, row_offset, volume_value_list):
 	name = filename
+	#with open(name, 'r', encoding="utf8") as filename_open:
 	with open(name, 'r', encoding="utf8") as filename_open:
 		f = csv.reader(filename_open)
 		filename_open.seek(0)
 		row1 = next(f)
 		volume_index = []
 		value_result = [('Null',)]*len(row_offset)
+		#value_result = []
 		attribute_tuple = []
 		attribute_name_list = []
 
@@ -223,14 +224,21 @@ def Findvalueincsv_offset(filename, row_offset, volume_value_list):
 				if row1[m] == value:
 					volume_index.append(m)
 					attribute_name_list.append(value)
-		for j, ind in enumerate(row_offset):
+		for m, ind in enumerate(row_offset):
 			filename_open.seek(ind)
-			row = next(filename_open)
-			for j, h in enumerate(volume_index):
-				if row[h] != '':
-					value_result.append(row[h])
-				else:
-					value_result.append('')
+			row = [next(filename_open)]
+			for row_list in reader(row):
+			#row_list = csv.reader(row)
+			#row_list =
+			#row_list = re.split(r',',row)
+				for j, h in enumerate(volume_index):
+					# print(row[h])
+					if row_list[h] != '':
+						#print(row_list[h])
+						value_result[m] = [row_list[h]]
+						#value_result.append(row_list[h])
+				# else:
+				# 	value_result[j] = ('Null',)
 		if volume_index == [] or value_result == []:
 			print("error: cannot find attribute in csv with referenced name or with the row indice")
 		# print(volume_value_list)
@@ -289,10 +297,8 @@ def FindValueinMultipleCsv(csv_list, tuplelist_for_csvs, attribute_list):
 	return final_attributelist, final_result
 
 
-# if __name__ == '__main__':
-# 	Main()
-
-
+if __name__ == '__main__':
+	Main()
 
 
 
